b8eaeba2c028c33f6c14495cb151fb3e4b643c83
Netinima <sirnetinima@gmail.com>
2026-03-18 16:04:10 +0000

    low key givi up

--- Game.py
+++ Game.py
@@ -17,9 +17,20 @@ while room == 0:
         print("(1)KYS  (2)Try and escape")
         room = int(input())
         continue
-if room == 1:
-    print("you died")
-elif room == 2:
-    print("What do you want to do now")
-    break
-    
+while room in [1,2]:
+    if room == 1:
+        print("you died")
+        room = int(input("Do you want to play again?"
+        " (1)Yes (2)No"))
+        if input == 1:
+            room = 0
+        elif input == 2:
+            print("Thanks for playing")
+            break
+        else:
+            print("choose 1 or 2")
+            room = int(input("Do you want to play again?"
+            " (1)Yes (2)No"))
+    elif room == 2:
+        print("What do you want to do now")
+        
